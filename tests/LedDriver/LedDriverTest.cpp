9c30ddc5f199204ede3163fc9260f1069b352a01
nanakira <nanakira@users.noreply.github.com>
2016-01-30 20:07:50 +0900

    Add LedDriverTest (dummy)

---
+++ tests/LedDriver/LedDriverTest.cpp
@@ -0,0 +1,18 @@
+#include "CppUTest/TestHarness.h"
+
+//START: NoFailStartingPoint
+TEST_GROUP(LedDriver)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+TEST(LedDriver, Create)
+{
+}
+//END: NoFailStartingPoint
