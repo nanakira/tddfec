5d033f5b209db6a4a6b8b2c8e02ab7604ab38302
nanakira <nanakira@users.noreply.github.com>
2016-02-13 20:13:25 +0900

    Test Id boundary

--- tests/HomeAutomation/LightSchedulerTest.cpp
+++ tests/HomeAutomation/LightSchedulerTest.cpp
@@ -206,6 +206,19 @@ TEST(LightScheduler, RemoveMultipleScheduledEvent)
     checkLightState(7, LIGHT_ON);
 }
 
+TEST(LightScheduler, AcceptsValidLightIds)
+{
+    LONGS_EQUAL(LS_OK, LightScheduler_ScheduleTurnOn(0, MONDAY, 600));
+    LONGS_EQUAL(LS_OK, LightScheduler_ScheduleTurnOn(15, MONDAY, 600));
+    LONGS_EQUAL(LS_OK, LightScheduler_ScheduleTurnOn(31, MONDAY, 600));
+}
+
+TEST(LightScheduler, RejectsInvalidLightIds)
+{
+    LONGS_EQUAL(LS_ID_OUT_OF_BOUNDS, LightScheduler_ScheduleTurnOn(-1, MONDAY, 600));
+    LONGS_EQUAL(LS_ID_OUT_OF_BOUNDS, LightScheduler_ScheduleTurnOn(32, MONDAY, 600));
+}
+
 TEST_GROUP(LightSchedulerInitAndCleanup)
 {
 };
