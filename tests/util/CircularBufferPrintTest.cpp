44af2d3a24f4ab639bea0f34ae0aa4808aa929db
nanakira <nanakira@users.noreply.github.com>
2016-02-17 06:57:11 +0900

    Add circular buffer - skipped

---
+++ tests/util/CircularBufferPrintTest.cpp
@@ -0,0 +1,71 @@
+//- ------------------------------------------------------------------
+//-    Copyright (c) James W. Grenning -- All Rights Reserved         
+//-    For use by owners of Test-Driven Development for Embedded C,   
+//-    and attendees of Renaissance Software Consulting, Co. training 
+//-    classes.                                                       
+//-                                                                   
+//-    Available at http://pragprog.com/titles/jgade/                 
+//-        ISBN 1-934356-62-X, ISBN13 978-1-934356-62-3               
+//-                                                                   
+//-    Authorized users may use this source code in your own          
+//-    projects, however the source code may not be used to           
+//-    create training material, courses, books, articles, and        
+//-    the like. We make no guarantees that this source code is       
+//-    fit for any purpose.                                           
+//-                                                                   
+//-    www.renaissancesoftware.net james@renaissancesoftware.net      
+//- ------------------------------------------------------------------
+
+
+
+
+#include "CppUTest/TestHarness.h"
+
+extern "C"
+{
+#include "CircularBuffer.h"
+#include "Utils.h"
+#include "FormatOutputSpy.h"
+}
+
+TEST_GROUP(CircularBufferPrint)
+{
+    CircularBuffer buffer;
+    const char * expectedOutput;
+    const char * actualOutput;
+
+    void setup()
+    {
+      UT_PTR_SET(FormatOutput, FormatOutputSpy);
+      FormatOutputSpy_Create(100);
+      actualOutput = FormatOutputSpy_GetOutput();
+      buffer = CircularBuffer_Create(10);
+    }
+
+    void teardown()
+    {
+       CircularBuffer_Destroy(buffer);
+       FormatOutputSpy_Destroy();
+    }
+};
+
+TEST(CircularBufferPrint, PrintEmpty)
+{
+    expectedOutput = "Circular buffer content:\n<>\n";
+    CircularBuffer_Print(buffer);
+    STRCMP_EQUAL(expectedOutput, actualOutput);
+}
+
+/*
+ * You should have only printed the heading.  this next test drives you
+ * to get the format of a single element.  Did you implement any of
+ * the wrap around logic in your print function?  Delete it if you did.
+ */
+
+TEST(CircularBufferPrint, PrintAfterOneIsPut)
+{
+    expectedOutput = "Circular buffer content:\n<17>\n";
+    CircularBuffer_Put(buffer, 17);
+    CircularBuffer_Print(buffer);
+    STRCMP_EQUAL(expectedOutput, actualOutput);
+}
