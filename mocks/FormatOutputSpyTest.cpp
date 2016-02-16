03d257b8edd6d429681f1ff861d7882237010ab3
nanakira <nanakira@users.noreply.github.com>
2016-02-16 22:48:37 +0900

    Add tests for FormatOutputSpy

--- mocks/FormatOutputSpyTest.cpp
+++ mocks/FormatOutputSpyTest.cpp
@@ -44,3 +44,26 @@ TEST(FormatOutputSpy, HelloWorld)
     STRCMP_EQUAL("Hello, World\n", FormatOutputSpy_GetOutput());
 }
 
+TEST(FormatOutputSpy, LimitTheOutputBufferSize)
+{
+    FormatOutputSpy_Create(4);
+    FormatOutput("Hello, World\n");
+    STRCMP_EQUAL("Hell", FormatOutputSpy_GetOutput());
+}
+
+TEST(FormatOutputSpy, PrintMultipleTimes)
+{
+    FormatOutputSpy_Create(25);
+    FormatOutput("Hello");
+    FormatOutput(", World\n");
+    STRCMP_EQUAL("Hello, World\n", FormatOutputSpy_GetOutput());
+}
+
+TEST(FormatOutputSpy, PrintMultipleOutputsPastFull)
+{
+    FormatOutputSpy_Create(12);
+    FormatOutput("12345");
+    FormatOutput("67890");
+    FormatOutput("ABCDEF");
+    STRCMP_EQUAL("1234567890AB", FormatOutputSpy_GetOutput());
+}
