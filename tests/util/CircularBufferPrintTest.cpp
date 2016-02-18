feb888422a221ccb970f8f2b205fe704d1bae53d
nanakira <nanakira@users.noreply.github.com>
2016-02-18 23:16:30 +0900

    Add more tests for circular buffer print

--- tests/util/CircularBufferPrintTest.cpp
+++ tests/util/CircularBufferPrintTest.cpp
@@ -69,3 +69,32 @@ TEST(CircularBufferPrint, PrintAfterOneIsPut)
     CircularBuffer_Print(buffer);
     STRCMP_EQUAL(expectedOutput, actualOutput);
 }
+
+TEST(CircularBufferPrint, PrintNotYetWrappedOrFull)
+{
+    expectedOutput = "Circular buffer content:\n<10, 20, 30>\n";
+    CircularBuffer_Put(buffer, 10);
+    CircularBuffer_Put(buffer, 20);
+    CircularBuffer_Put(buffer, 30);
+    CircularBuffer_Print(buffer);
+    STRCMP_EQUAL(expectedOutput, actualOutput);
+}
+
+TEST(CircularBufferPrint, PrintNotYetWrappedAndIsFull)
+{
+    expectedOutput = "Circular buffer content:\n"
+                     "<31, 41, 59, 26, 53>\n";
+
+    CircularBuffer b = CircularBuffer_Create(5);
+    CircularBuffer_Put(b, 31);
+    CircularBuffer_Put(b, 41);
+    CircularBuffer_Put(b, 59);
+    CircularBuffer_Put(b, 26);
+    CircularBuffer_Put(b, 53);
+
+    CircularBuffer_Print(b);
+
+    STRCMP_EQUAL(expectedOutput, actualOutput);
+    CircularBuffer_Destroy(b);
+}
+
