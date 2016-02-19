7ca117763b3d35348ba1d68b8ea8e2d2482b84fb
nanakira <nanakira@users.noreply.github.com>
2016-02-19 23:19:02 +0900

    First implementation of mock for IO Flash

---
+++ tests/IO/FlashTest.cpp
@@ -0,0 +1,65 @@
+//- ------------------------------------------------------------------
+//-    Copyright (c) James W. Grenning -- All Rights Reserved         
+//-    For use by owners of Test-Driven Development for Embedded C,   
+//-    and attendees of Renaissance Software Consulting, Co. training 
+//-    classes.                                                       
+//-                                                                   
+//-    Available at http://pragprog.com/titles/jgade/                 
+//-        ISBN 1-934356-62-X, ISBN13 978-1-934356-62-3               
+//-                                                                   
+//-    Authorized users may use this source code in your own          
+//-    projects, however the source code may not be used to           
+//-    create training material, courses, books, articles, and        
+//-    the like. We make no guarantees that this source code is       
+//-    fit for any purpose.                                           
+//-                                                                   
+//-    www.renaissancesoftware.net james@renaissancesoftware.net      
+//- ------------------------------------------------------------------
+
+
+extern "C"
+{
+#include "Flash.h"
+#include "MockIO.h"
+#include "m28w160ect.h"
+}
+
+#include "CppUTest/TestHarness.h"
+
+//START: TEST_GROUP_with_MicroTime
+TEST_GROUP(Flash)
+{
+    ioAddress address;
+    ioData data;
+    int result;
+    int timeoutInMicroseconds;
+    int fakeMicroTimeIncrement;
+
+    void setup()
+    {
+        address = 0x1000;
+        data = 0xBEEF;
+        result = -1;
+        MockIO_Create(20);
+//        FakeMicroTime_Init(0, 1);
+        Flash_Create();
+    }
+
+    void teardown()
+    {
+        Flash_Destroy();
+        MockIO_Verify_Complete();
+        MockIO_Destroy();
+    }
+};
+
+
+TEST(Flash, WriteSucceeds_ReadyImmediately)
+{
+    MockIO_Expect_Write(CommandRegister, ProgramCommand);
+    MockIO_Expect_Write(address, data);
+    MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit);
+    MockIO_Expect_ReadThenReturn(address, data);
+    result = Flash_Write(address, data);
+    LONGS_EQUAL(FLASH_SUCCESS, result);
+}
