4d30daf7c91b79ec430c0294e800a86827ba9c0c
nanakira <nanakira@users.noreply.github.com>
2016-02-21 20:37:37 +0900

    Flash_Write wait until ready

--- tests/IO/FlashTest.cpp
+++ tests/IO/FlashTest.cpp
@@ -63,3 +63,17 @@ TEST(Flash, WriteSucceeds_ReadyImmediately)
     result = Flash_Write(address, data);
     LONGS_EQUAL(FLASH_SUCCESS, result);
 }
+
+TEST(Flash, SucceedsNotImmediatelyReady)
+{
+    MockIO_Expect_Write(CommandRegister, ProgramCommand);
+    MockIO_Expect_Write(address, data);
+    MockIO_Expect_ReadThenReturn(StatusRegister, 0);
+    MockIO_Expect_ReadThenReturn(StatusRegister, 0);
+    MockIO_Expect_ReadThenReturn(StatusRegister, 0);
+    MockIO_Expect_ReadThenReturn(StatusRegister, ReadyBit);
+    MockIO_Expect_ReadThenReturn(address, data);
+
+    result = Flash_Write(address, data);
+    LONGS_EQUAL(FLASH_SUCCESS, result);
+}
