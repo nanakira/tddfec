83c6fa376fb2e2db283e37fcbd8f137678d9541e
nanakira <nanakira@users.noreply.github.com>
2016-02-24 22:17:05 +0900

    Refactor code

--- src/IO/Flash.c
+++ src/IO/Flash.c
@@ -40,6 +40,19 @@ void Flash_Destroy(void)
 {
 }
 
+static int writeError(int status)
+{
+    IO_Write(CommandRegister, Reset);
+    if (status & VppErrorBit)
+        return FLASH_VPP_ERROR;
+    else if (status & ProgramErrorBit)
+        return FLASH_PROGRAM_ERROR;
+    else if (status & BlockProtectionErrorBit)
+        return FLASH_PROTECTED_BLOCK_ERROR;
+    else
+        return FLASH_UNKNOWN_PROGRAM_ERROR;
+}
+
 int Flash_Write(ioAddress address, ioData data)
 {
     ioData status = 0;
@@ -51,18 +64,7 @@ int Flash_Write(ioAddress address, ioData data)
         status = IO_Read(StatusRegister);
 
     if (status != ReadyBit)
-    {
-        IO_Write(CommandRegister, Reset);
-
-        if (status & VppErrorBit)
-            return FLASH_VPP_ERROR;
-        else if (status & ProgramErrorBit)
-            return FLASH_PROGRAM_ERROR;
-        else if (status & BlockProtectionErrorBit)
-            return FLASH_PROTECTED_BLOCK_ERROR;
-        else
-            return FLASH_UNKNOWN_PROGRAM_ERROR;
-    }
+        return writeError(status);
     if (data != IO_Read(address))
         return FLASH_READ_BACK_ERROR;
     return FLASH_SUCCESS;
